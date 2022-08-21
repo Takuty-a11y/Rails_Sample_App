824cdbb2eba2e97af52ee79327124423d08ceb38
takuty-a11y <takuty.008.awenite.1121@gmail.com>
2022-08-21 23:52:12 +0000

    Change Mailgun Setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'secret-plains-57111.herokuapp.com'
+  host = 'https://secret-plains-57111.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
